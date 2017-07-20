224fdd8f517c411016eca80805257a05de3936d8
jakekausler <jakekausler@yahoo.com>
2017-07-20 19:40:40 -0400

    Only use defaults on new books

--- web/app/main/bookviews/editor/editorcontroller.js
+++ web/app/main/bookviews/editor/editorcontroller.js
@@ -1,137 +1,85 @@
 angular.module('libraryOrganizer')
 .controller('editorController', function($scope, $http, $mdDialog, book, $vm, viewType, username) {
 	$scope.book = angular.copy(book);
-	if (!$scope.book.title) {
+	if (!$scope.book.bookid) {
 		$vm.getSettingByName('Title', function(value) {
 			$scope.book.title = value;
 		});
-	}
-	if (!$scope.book.subtitle) {
 		$vm.getSettingByName('Subtitle', function(value) {
 			$scope.book.subtitle = value;
 		});
-	}
-	if (!$scope.book.series) {
 		$vm.getSettingByName('Series', function(value) {
 			$scope.book.series = value;
 		});
-	}
-	if (!$scope.book.volume) {
 		$vm.getSettingByName('Volume', function(value) {
 			$scope.book.volume = value;
 		});
-	}
-	if (!$scope.book.imageurl) {
 		$vm.getSettingByName('ImageURL', function(value) {
 			$scope.book.imageurl = value;
 		});
-	}
-	if (!$scope.book.publisher.publisher) {
 		$vm.getSettingByName('Publisher', function(value) {
 			$scope.book.publisher.publisher = value;
 		});
-	}
-	if (!$scope.book.publisher.city) {
 		$vm.getSettingByName('City', function(value) {
 			$scope.book.publisher.city = value;
 		});
-	}
-	if (!$scope.book.publisher.state) {
 		$vm.getSettingByName('State', function(value) {
 			$scope.book.publisher.state = value;
 		});
-	}
-	if (!$scope.book.publisher.country) {
 		$vm.getSettingByName('Country', function(value) {
 			$scope.book.publisher.country = value;
 		});
-	}
-	if (!$scope.book.originallypublished) {
 		$vm.getSettingByName('Originally Published', function(value) {
 			$scope.book.originallypublished = value;
 		});
-	}
-	if (!$scope.book.editionpublished) {
 		$vm.getSettingByName('Edition Published', function(value) {
 			$scope.book.editionpublished = value;
 		});
-	}
-	if (!$scope.book.dewey) {
 		$vm.getSettingByName('Dewey', function(value) {
 			$scope.book.dewey = value;
 		});
-	}
-	if (!$scope.book.format) {
 		$vm.getSettingByName('Format', function(value) {
 			$scope.book.format = value;
 		});
-	}
-	if (!$scope.book.pages) {
 		$vm.getSettingByName('Pages', function(value) {
 			$scope.book.pages = value;
 		});
-	}
-	if (!$scope.book.width) {
 		$vm.getSettingByName('Width', function(value) {
 			$scope.book.width = value;
 		});
-	}
-	if (!$scope.book.height) {
 		$vm.getSettingByName('Height', function(value) {
 			$scope.book.height = value;
 		});
-	}
-	if (!$scope.book.depth) {
 		$vm.getSettingByName('Depth', function(value) {
 			$scope.book.depth = value;
 		});
-	}
-	if (!$scope.book.weight) {
 		$vm.getSettingByName('Weight', function(value) {
 			$scope.book.weight = value;
 		});
-	}
-	if (!$scope.book.primarylanguage) {
 		$vm.getSettingByName('Primary Language', function(value) {
 			$scope.book.primarylanguage = value;
 		});
-	}
-	if (!$scope.book.secondarylanguage) {
 		$vm.getSettingByName('Secondary Language', function(value) {
 			$scope.book.secondarylanguage = value;
 		});
-	}
-	if (!$scope.book.originallanguage) {
 		$vm.getSettingByName('Original Language', function(value) {
 			$scope.book.originallanguage = value;
 		});
-	}
-	if (!$scope.book.isowned) {
 		$vm.getSettingByName('Owned', function(value) {
 			$scope.book.isowned = (value=="True");
 		});
-	}
-	if (!$scope.book.isread) {
 		$vm.getSettingByName('Read', function(value) {
 			$scope.book.isread = (value=="True");
 		});
-	}
-	if (!$scope.book.isreference) {
 		$vm.getSettingByName('Reference', function(value) {
 			$scope.book.isreference = (value=="True");
 		});
-	}
-	if (!$scope.book.isshipping) {
 		$vm.getSettingByName('Shipping', function(value) {
 			$scope.book.isshipping = (value=="True");
 		});
-	}
-	if (!$scope.book.isreading) {
 		$vm.getSettingByName('Reading', function(value) {
 			$scope.book.isreading = (value=="True");
 		});
-	}
-	if (!$scope.book.edition) {
 		$vm.getSettingByName('Edition', function(value) {
 			$scope.book.edition = value;
 		});
